de807fcd0abc53f82163f86551d8c96727ca94fe
xfreya97 <freja.s@hotmail.com>
2014-12-01 15:04:07 +0100

    Spel
    Hej

---
+++ Programmering/Multi-Dimensional Arrays in Java/src/prjstrings/StringManipulation.java
@@ -0,0 +1,20 @@
+
+
+package prjstrings;
+
+
+public class StringManipulation {
+    
+    public static void main (String[] args) {
+        
+        int result; 
+        String Word1 = "Ape";
+        String Word2 = "App";
+        
+        result = Word1.compareTo(Word2);
+        
+        if (result <0) 
+        
+    }
+    
+}
